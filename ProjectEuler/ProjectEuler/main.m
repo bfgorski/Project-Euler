c639a9606ffeb074bb3cf094ef94c5f716a3502a
Ben Gregorski <bfgorski@gmail.com>
2012-02-05 01:10:59 -0800

    Adding XCode project

---
+++ ProjectEuler/ProjectEuler/main.m
@@ -0,0 +1,19 @@
+//
+//  main.cpp
+//  ProjectEuler
+//
+//  Created by Benjamin Gregorski on 8/18/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
+#include "prob0012.h"
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    solveProb0012();
+    return 0;
+}
+
